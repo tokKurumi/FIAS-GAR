4e707dfa75bf85e05e02ae075d5a92eacc5bb112
Oleg Yudashkin <tkurumi901@gmail.com>
2024-04-10 05:10:16 +0300

    The most significant changes involve the addition of two new classes, `XmlReaderCopyHelper<TModel>` and `XmlCopyHelpers`, which are designed to facilitate the mapping of XML attributes to model properties.
    1. The `XmlReaderCopyHelper<TModel>` class has been added to the `GAR.Services.ReaderApi.Core` namespace. This class is designed to map XML attributes to properties of a model class. It includes a dictionary to store these mappings and provides methods to add new mappings, read XML data asynchronously, and create model instances based on the XML data. The previously existing `SetProperty` method in this class has been removed and replaced with a static version that uses reflection to set a model instance's property.
    
    2. The `XmlCopyHelpers` class has been added to the `GAR.Services.ReaderApi.Data` namespace. This class provides an `Addresses` property that returns an instance of `XmlReaderCopyHelper<AddressObject>`. This instance is configured to map the `Id` and `ObjectId` attributes of an XML element to the corresponding properties of an `AddressObject` instance.
    
    Code Changes:
    - Addition of `XmlReaderCopyHelper<TModel>` class in `XmlReaderCopyHelper.cs` file.
    - Removal of old `SetProperty` method and addition of new static `SetProperty` method in `XmlReaderCopyHelper<TModel>` class.
    - Addition of `XmlCopyHelpers` class in `XmlCopyHelpers.cs` file.

--- src/GAR.Services.ReaderApi/Core/XmlReaderCopyHelper.cs
+++ src/GAR.Services.ReaderApi/Core/XmlReaderCopyHelper.cs
@@ -1,9 +1,59 @@
 ﻿namespace GAR.Services.ReaderApi.Core;
+
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Xml;
+
 public class XmlReaderCopyHelper<TModel>(
     string name)
 {
     private readonly string _name = name;
+    private readonly Dictionary<string, Expression<Func<TModel, object>>> _mappings = [];
 
+    public XmlReaderCopyHelper<TModel> Map(string attributeName, Expression<Func<TModel, object>> property)
+    {
+        _mappings.Add(attributeName, property);
+        return this;
+    }
 
-    private void SetProperty<TProperty>()
+    public async IAsyncEnumerator<TModel> GetAsync(XmlReader reader)
+    {
+        while (await reader.ReadAsync())
+        {
+            if (reader.NodeType == XmlNodeType.Element && reader.Name == _name)
+            {
+                var item = ReadItem(reader);
+                if (item is not null)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    private static void SetProperty<TProperty>(TModel item, Expression<Func<TModel, TProperty>> propertyLambda, string value)
+    {
+        if (propertyLambda.Body is MemberExpression memberExpression)
+        {
+            var property = (PropertyInfo)memberExpression.Member;
+            var propType = property.PropertyType;
+            var propValue = Convert.ChangeType(value, propType);
+            property.SetValue(item, propValue);
+        }
+    }
+
+    private TModel ReadItem(XmlReader reader)
+    {
+        var item = Activator.CreateInstance<TModel>();
+
+        foreach (var (attributeName, property) in _mappings)
+        {
+            var value = reader.GetAttribute(attributeName);
+            ArgumentException.ThrowIfNullOrEmpty(value, attributeName);
+
+            XmlReaderCopyHelper<TModel>.SetProperty(item, property, value);
+        }
+
+        return item;
+    }
 }
---
+++ src/GAR.Services.ReaderApi/Data/XmlCopyHelpers.cs
@@ -0,0 +1,12 @@
+﻿namespace GAR.Services.ReaderApi.Data;
+
+using GAR.Services.ReaderApi.Core;
+using GAR.Services.ReaderApi.Models;
+
+public class XmlCopyHelpers
+{
+    public XmlReaderCopyHelper<AddressObject> Addresses =>
+        new XmlReaderCopyHelper<AddressObject>(AddressObject.XmlElementName)
+            .Map(AddressObject.XmlNames.Id, ao => ao.Id)
+            .Map(AddressObject.XmlNames.ObjectId, ao => ao.ObjectId);
+}
