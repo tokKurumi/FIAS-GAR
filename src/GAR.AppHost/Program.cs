9ad49e32313a06a95abad1e29496d31eef1c1a2c
Oleg Yudashkin <tkurumi901@gmail.com>
2024-05-15 04:30:47 +0300

    `Add new parameter and update settings in Program.cs and appsettings.json`
     Added a new parameter `gar-database-postgres-container-password` to `Program.cs` for Postgres database password. The database is now associated with a data volume named `gar-volume`. In `appsettings.json`, reformatted the logging level settings and added a new `Parameters` section with the new parameter. Also, updated the `Folder` field value under `GAR` from `13` to `99`.

--- src/GAR.AppHost/Program.cs
+++ src/GAR.AppHost/Program.cs
@@ -1,8 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var databasePassword = builder.AddParameter("gar-database-postgres-container-password", true);
+
 var database = builder
-    .AddPostgres("gar-database-postgres-container")
+    .AddPostgres("gar-database-postgres-container", password: databasePassword)
     .WithPgAdmin()
+    .WithDataVolume("gar-volume")
     .AddDatabase("gar-database");
 
 var readerApi = builder
